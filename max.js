bd76b2284b46df8e5742e7d1be5b2fe1dc2aa676
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-08-12 11:46:04 -0700

    Update max.js

--- max.js
+++ max.js
@@ -7,11 +7,13 @@ function calculateMax() {
   let Fail_Value = [];
   let Zero_Counter = 0;
   let Zero = [];
-  
+
+  let msn = document.getElementById('msn').value;
   let D1 = parseFloat(document.getElementById('D1').value);
   let D2 = parseFloat(document.getElementById('D2').value);
   let D3 = parseFloat(document.getElementById('D3').value);
   let weight = parseFloat(document.getElementById('Weight').value);
+  let name ="";
   
   let X = D1-(weight*0.0000155);
   let Y = D2-(weight*0.0000155);
@@ -60,16 +62,28 @@ function calculateMax() {
     Passed = Passed + 1
     Pass_Value.push("H = "+H.toFixed(4))
   }
-  
-    let resultsHTML = "<h3>Results:</h3><br>";
-    resultsHTML += "<p class = 'recheck'>Total Rechecks: " + Zero_Counter + "</p><br>";
-    Zero.forEach(element => resultsHTML += "<p class = 'recheck'>" + element + "</p><br>");
-    
-    resultsHTML += "<p class = 'pass'>Total Passes: " + Passed + "</p><br>";
-    Pass_Value.forEach(element => resultsHTML += "<p class = 'pass'>" + element + "</p><br>");
-    
-    resultsHTML += "<p class = 'fail'>Total Fails: " + Failed + "</p><br>";
-    Fail_Value.forEach(element => resultsHTML += "<p class = 'fail'>" + element + "</p><br>");
-    
+  if (msn !== "") {
+    let name = msn;
+    let resultsHTML = "<h3 class='underline'>Freeplay Results for " + name + "</h3><br>";
+    resultsHTML += "<p class = 'pass'>Total Passed: " + Pass_Value.length + "</p><br>";
+    for (let i = 0; i < Pass_Value.length; i++) {
+      resultsHTML += "<p class = 'pass'>" + Pass_Value[i] + "</p><br>";
+    }
+    resultsHTML += "<p class = 'fail'>Total Failed: " + Fail_Value.length + "</p><br>";
+    for (let i = 0; i < Fail_Value.length; i++) {
+      resultsHTML += "<p class = 'fail'>" + Fail_Value[i] + "</p><br>";
+    }
     document.getElementById('results').innerHTML = resultsHTML;
+  } else {
+    let resultsHTML2 = "<h3 class='underline'>Freeplay Results</h3><br>";
+    resultsHTML2 += "<p class = 'pass'>Total Passed: " + Pass_Value.length + "</p><br>";
+    for (let i = 0; i < Pass_Value.length; i++) {
+      resultsHTML2 += "<p class = 'pass'>" + Pass_Value[i] + "</p><br>";
+    }
+    resultsHTML2 += "<p class = 'fail'>Total Failed: " + Fail_Value.length + "</p><br>";
+    for (let i = 0; i < Fail_Value.length; i++) {
+      resultsHTML2 += "<p class = 'fail'>" + Fail_Value[i] + "</p><br>";
+    }
+    document.getElementById('results').innerHTML = resultsHTML2;
+  }
 }
