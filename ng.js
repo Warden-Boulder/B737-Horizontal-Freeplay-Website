e21e367eaaea07c5a473d2565dc9acd2a6504b42
Boulder <157924017+Warden-Boulder@users.noreply.github.com>
2025-08-21 15:27:44 -0700

    Update ng.js

--- ng.js
+++ ng.js
@@ -12,29 +12,93 @@ function calculateNG() {
   let name = "";
 
   if (D1 > limits[0]) {
-    Fail.push("D1 = " + D1.toFixed(4));
-  } else {
-    Pass.push("D1 = " + D1.toFixed(4));
+    let d1 = D1.toFixed(4); 
+    if (d1.endsWith('00')) {
+      Fail.push("D1 = "+ D1.toFixed(2));
+    } else if (d1.endsWith('0')) {
+      Fail.push("D1 = "+ D1.toFixed(3));
+    } else {
+      Fail.push("D1 = "+ D1.toFixed(4));
+    }
+  }
+
+  if (D1 <= limits[0]) {
+    let d1 = D1.toFixed(4); 
+    if (d1.endsWith('00')) {
+      Pass.push("D1 = "+ D1.toFixed(2));
+    } else if (d1.endsWith('0')) {
+      Pass.push("D1 = "+ D1.toFixed(3));
+    } else {
+      Pass.push("D1 = "+ D1.toFixed(4));
+    }
   }
 
   if (D2 > limits[1]) {
-    Fail.push("D2 = " + D2.toFixed(4));
-  } else {
-    Pass.push("D2 = " + D2.toFixed(4));
+    let d2 = D2.toFixed(4); 
+    if (d2.endsWith('00')) {
+      Fail.push("D2 = "+ D2.toFixed(2));
+    } else if (d2.endsWith('0')) {
+      Fail.push("D2 = "+ D2.toFixed(3));
+    } else {
+      Fail.push("D2 = "+ D2.toFixed(4));
+    }
+  }
+
+  if (D2 <= limits[1]) {
+    let d2 = D2.toFixed(4); 
+    if (d2.endsWith('00')) {
+      Pass.push("D2 = "+ D2.toFixed(2));
+    } else if (d2.endsWith('0')) {
+      Pass.push("D2 = "+ D2.toFixed(3));
+    } else {
+      Pass.push("D2 = "+ D2.toFixed(4));
+    }
   }
 
   if (D3 > limits[2]) {
-    Fail.push("D3 = " + D3.toFixed(4));
-  } else {
-    Pass.push("D3 = " + D3.toFixed(4));
+    let d3 = D3.toFixed(4); 
+    if (d3.endsWith('00')) {
+      Fail.push("D3 = "+ D3.toFixed(2));
+    } else if (d3.endsWith('0')) {
+      Fail.push("D3 = "+ D3.toFixed(3));
+    } else {
+      Fail.push("D3 = "+ D3.toFixed(4));
+    }
+  }
+
+  if (D3 <= limits[2]) {
+    let d3 = D3.toFixed(4); 
+    if (d3.endsWith('00')) {
+      Pass.push("D3 = "+ D3.toFixed(2));
+    } else if (d3.endsWith('0')) {
+      Pass.push("D3 = "+ D3.toFixed(3));
+    } else {
+      Pass.push("D3 = "+ D3.toFixed(4));
+    }
   }
 
   if (H > limits[3]) {
-    Fail.push("H = " + H.toFixed(4));
-  } else {
-    Pass.push("H = " + H.toFixed(4));
+    let h = H.toFixed(4); 
+    if (h.endsWith('00')) {
+      Fail.push("H = "+ H.toFixed(2));
+    } else if (h.endsWith('0')) {
+      Fail.push("H = "+ H.toFixed(3));
+    } else {
+      Fail.push("H = "+ H.toFixed(4));
+    }
   }
 
+  if (H <= limits[3]) {
+    let h = H.toFixed(4); 
+    if (h.endsWith('00')) {
+      Pass.push("H = "+ H.toFixed(2));
+    } else if (h.endsWith('0')) {
+      Pass.push("H = "+ H.toFixed(3));
+    } else {
+      Pass.push("H = "+ H.toFixed(4));
+    }
+  }
+  
   if (msn !== "") {
     let name = msn;
     let resultsHTML = "<h3 class='results underline'>Freeplay Results for " + name + "</h3><br>";
